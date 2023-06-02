b5b12ea490bfcdbe60a78bc78a8b37c4db748fe9
mikersays <129307237+mikersays@users.noreply.github.com>
2023-06-02 00:33:26 -0700

    Create discochatty.py

---
+++ discochatty.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+import openai
+
+openai.api_key = '<openAI API Key'
+
+intents = discord.Intents.all()  # Enables all intents
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.command()
+async def chat(ctx, *, message):
+    response = openai.ChatCompletion.create(
+      model="gpt-4", # assuming that the GPT-4 model is named "gpt-4"
+      messages=[
+            {
+                "role": "system",
+                "content": "You are a helpful assistant."
+            },
+            {
+                "role": "user",
+                "content": message
+            }
+        ]
+    )
+    await ctx.send(response['choices'][0]['message']['content'])
+
+bot.run('<Discord bot token>')
