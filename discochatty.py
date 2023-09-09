1cc98ea87e73fb88469ca4c1fa26814174a3fbe4
mikersays <129307237+mikersays@users.noreply.github.com>
2023-09-09 15:30:06 -0700

    Update discochatty.py

--- discochatty.py
+++ discochatty.py
@@ -12,7 +12,7 @@ async def chat(ctx, *, message, max_tokens=1500, temperature=1):
     for _ in range(2):  # two attempts in total: one original and one retry
         try:
             response = openai.ChatCompletion.create(
-                model="gpt-4-32k-0613",
+                model="gpt-4",
                 max_tokens=max_tokens,
                 temperature=temperature,
                 n=1,
